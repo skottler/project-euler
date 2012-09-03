8f54b3e4c00200179429ebc9d3988ce436a91be8
Sam Kottler <shk@redhat.com>
2012-09-03 13:31:42 -0400

    Added the first solution

---
+++ projecteuler1.java
@@ -0,0 +1,14 @@
+import java.lang.String;
+import java.lang.System;
+
+public class projecteuler1 {
+    public static void main(String[] args) {
+        int total = 0;
+        for (int i=0; i < 1000; i++) {
+            if (i % 3 == 0 || i % 5 == 0) {
+                total += i;
+            }
+        }
+        System.out.println(total);
+    }
+}
